b41dd609112ad544077da1d795980ccf2e07dc0a
Andy Maloney <asmaloney@gmail.com>
2018-03-22 12:37:13 -0400

    Move default argument to the declaration

--- src/tools/las2e57.cpp
+++ src/tools/las2e57.cpp
@@ -404,12 +404,12 @@ struct GroupRecord {
     int64_t     startIndex;
     BoundingBox bbox;
 
-                GroupRecord(int64_t id);
+                GroupRecord(int64_t id = 0);
     void        addMember(double coords[3], int64_t recordIndex);
     void        dump(int indent = 0, std::ostream& os = std::cout);
 };
 
-GroupRecord::GroupRecord(int64_t id_arg = 0)
+GroupRecord::GroupRecord(int64_t id_arg)
 : id(id_arg),
   count(0),
   startIndex(0),

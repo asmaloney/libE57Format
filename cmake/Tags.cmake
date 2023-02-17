7cd91062629cdc0153a8f85e1475570788bf2d98
Andy Maloney <asmaloney@gmail.com>
2023-02-17 09:34:28 -0500

    {cmake} Rewrite Tags.cmake (#228)
    - include clang detection
    - MSVC now uses CMake's MSVC_VERSION instead of trying to enumerate versions
    - defaults to using the CMake compiler ID if we aren't special casing the complier info

--- cmake/Tags.cmake
+++ cmake/Tags.cmake
@@ -1,41 +1,38 @@
 # This file defines the variables
 # ${PROJECT_NAME}_BUILD_TAG
 
-# calculate the tag
-set(T_ ${CMAKE_SYSTEM_PROCESSOR})
-if (CMAKE_CL_64)
-    set(T_ ${T_}_64)
-endif (CMAKE_CL_64)
-string(TOLOWER ${CMAKE_SYSTEM_NAME} T1_)
-set(T_ ${T_}-${T1_})
-if (MSVC90)
-    set(T1_ "-vc90")
-elseif (MSVC10)
-    set(T1_ "-vc100")
-elseif (MSVC80)
-    set(T1_ "-vc80")
-elseif (MSVC71)
-    set(T1_ "-vc711")
-elseif (MSVC70)
-    set(T1_ "-vc7")
-elseif (MINGW)
-    set(T1_ "-mgw")
-    exec_program(${CMAKE_CXX_COMPILER}
+set( T_ ${CMAKE_SYSTEM_PROCESSOR} )
+
+string( TOLOWER ${CMAKE_SYSTEM_NAME} T1_ )
+
+function( add_compiler_version )
+    exec_program( ${CMAKE_CXX_COMPILER}
         ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
         OUTPUT_VARIABLE T2_
     )
-    string(REGEX REPLACE "([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2" T2_ ${T2_})
-    set(T1_ ${T1_}${T2_})
-elseif (CMAKE_COMPILER_IS_GNUCXX)
-    set(T1_ "-gcc")
-    exec_program(${CMAKE_CXX_COMPILER}
-        ARGS ${CMAKE_CXX_COMPILER_ARG1} -dumpversion
-        OUTPUT_VARIABLE T2_
-    )
-    string(REGEX REPLACE "([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2" T2_ ${T2_})
-    set(T1_ ${T1_}${T2_})
-else()
-    set(T1_)
+    string( REGEX REPLACE "([0-9])\\.([0-9])(\\.[0-9])?" "\\1\\2" T2_ ${T2_} )
+    set( T1_ ${T1_}${T2_} PARENT_SCOPE )
+endfunction()
+
+# default to just the CMake compiler ID
+set( T1_ ${CMAKE_CXX_COMPILER_ID} )
+
+# special cases to add versions and other info
+if ( MSVC )
+    if ( CMAKE_CL_64 )
+        set( T_ ${T_}_64 )
+    endif()
+
+    set( T1_ "vc${MSVC_VERSION}" )
+elseif ( MINGW )
+    set( T1_ "MinGW" )
+    add_compiler_version()
+elseif ( CMAKE_CXX_COMPILER_ID STREQUAL "GNU" )
+    set( T1_ "gcc" )
+    add_compiler_version()
+elseif ( CMAKE_CXX_COMPILER_ID MATCHES "Clang")
+    add_compiler_version()
 endif()
-set(T_ ${T_}${T1_})
-set(${PROJECT_NAME}_BUILD_TAG ${T_})
+
+set( T_ ${T_}-${T1_} )
+set( ${PROJECT_NAME}_BUILD_TAG ${T_} )

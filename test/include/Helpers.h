1abd4e91beb452aaa0e402bfcd6ac32f237a3b61
Andy Maloney <asmaloney@gmail.com>
2024-06-20 16:40:59 -0400

    {test} Fix warnings about extraneous semicolons

--- test/include/Helpers.h
+++ test/include/Helpers.h
@@ -4,6 +4,8 @@
 
 // GoogleTest's ASSERT_NO_THROW() doesn't let us show any info about the exceptions.
 // This wrapper macro will output the e57::E57Exception context on failure.
+// The static_assert is simply there to require a semicolon after the macro so it matches the
+// E57_ASSERT_THROW() case.
 #define E57_ASSERT_NO_THROW( code )                                                                \
    try                                                                                             \
    {                                                                                               \
@@ -12,7 +14,8 @@
    catch ( e57::E57Exception & err )                                                               \
    {                                                                                               \
       FAIL() << err.errorStr() << ": " << err.context();                                           \
-   }
+   }                                                                                               \
+   static_assert( true, "" )
 
 #define E57_ASSERT_THROW( code ) ASSERT_THROW( code, e57::E57Exception )
 
